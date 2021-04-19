2747bcd241f69f3030916253b85f46d6f3d007d3
alexSmkh <alexshikari9333@gmail.com>
2021-04-19 22:29:02 +0300

    Added the services section

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import AboutSection from '../components/AboutSection';
+import ServicesSection from '../components/ServicesSection';
 
 const AboutUs = () => (
-  <AboutSection />
+  <>
+    <AboutSection />
+    <ServicesSection />
+  </>
 );
 
 export default AboutUs;
