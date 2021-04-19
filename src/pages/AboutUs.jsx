044335102a4cb29a922534e62da9e834b50c874d
alexSmkh <alexshikari9333@gmail.com>
2021-04-19 22:59:40 +0300

    Added the faq section

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import AboutSection from '../components/AboutSection';
 import ServicesSection from '../components/ServicesSection';
+import Faq from '../components/FaqSection';
 
 const AboutUs = () => (
   <>
     <AboutSection />
     <ServicesSection />
+    <Faq />
   </>
 );
 
