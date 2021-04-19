d5b8c36dc4b8ccf644c2c0efe297201c9e2a3446
alexSmkh <alexshikari9333@gmail.com>
2021-04-19 23:45:53 +0300

    Styled the about section

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,34 +1,69 @@
 import React from 'react';
+import styled from 'styled-components';
+
 import home1 from '../img/home1.png';
 
+const About = styled.div`
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem 10rem;
+  color: white;
+`;
+
+const Description = styled.div`
+  flex: 1;
+  padding-right: 5rem;
+
+  h2 {
+    font-weight: lighter;
+  }
+`;
+
+const Image = styled.div`
+  flex: 1;
+  overflow: hidden;
+
+  img {
+    width: 100%;
+    height: 80vh;
+    object-fit: cover;
+  }
+`;
+
+const Hide = styled.div`
+  overflow: hidden;
+`;
+
 const AboutSection = () => (
-  <div className="about">
-    <div className="description">
+  <About>
+    <Description>
       <div className="title">
-        <div className="hide">
+        <Hide>
           <h2>We work to make</h2>
-        </div>
-        <div className="hide">
+        </Hide>
+        <Hide>
           <h2>
             your&nbsp;
             <span>dreams</span>
             &nbsp;come
           </h2>
-        </div>
-        <div className="hide">
+        </Hide>
+        <Hide>
           <h2>true.</h2>
-        </div>
+        </Hide>
       </div>
       <p>
         Contact us for any photography or videography ideas that you have. We
         have professionals with amazing skills to help you achieve it.
       </p>
       <button type="button">Contant Us</button>
-    </div>
-    <div className="image">
+    </Description>
+    <Image>
       <img src={home1} alt="photographer" />
-    </div>
-  </div>
+    </Image>
+  </About>
 );
 
 export default AboutSection;
