b35faf09502029220bfeaa3bac664c39ce18ef59
alexSmkh <alexshikari9333@gmail.com>
2021-04-24 00:53:21 +0300

    Styled the contact page

--- src/animation.js
+++ src/animation.js
@@ -132,3 +132,19 @@ export const scrollReveal = {
     },
   },
 };
+
+export const iconAnim = {
+  hidden: {
+    opacity: 0,
+    scale: 1.2,
+    x: '-100%',
+  },
+  show: {
+    opacity: 1,
+    scale: 1,
+    x: '0%',
+    transition: {
+      duration: 1,
+    },
+  },
+};
--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,18 +1,123 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-import { pageAnimation } from '../animation';
+import styled from 'styled-components';
+
+import {
+  pageAnimation, titleAnim, lineAnim, iconAnim,
+} from '../animation';
+import twitterLogo from '../img/twitter-logo.svg';
+import fbLogo from '../img/facebook-logo.svg';
+import gmailLogo from '../img/gmail-logo.svg';
+import photographer from '../img/photographer.jpg';
 
 function ContactUs() {
   return (
-    <motion.div
+    <StyledContact
       variants={pageAnimation}
       initial="hidden"
       animate="show"
       exit="exit"
     >
-      <h1>Our Contacts</h1>
-    </motion.div>
+      <Title>
+        <Hide>
+          <motion.h2 variants={titleAnim}>Get in touch.</motion.h2>
+        </Hide>
+      </Title>
+      <Line variants={lineAnim} />
+      <div className="contacts">
+        <Hide>
+          <Social>
+            <Circle variants={iconAnim}>
+              <Icon src={gmailLogo} alt="gmail-logo" />
+            </Circle>
+            <Link variants={titleAnim} href="https://gmail.com" target="_blank">
+              Send Us An Email
+            </Link>
+          </Social>
+        </Hide>
+        <Hide>
+          <Social>
+            <Circle variants={iconAnim}>
+              <Icon src={fbLogo} alt="facebook-logo" />
+            </Circle>
+            <Link
+              variants={titleAnim}
+              href="https://facebook.com"
+              target="_blank"
+            >
+              Facebook
+            </Link>
+          </Social>
+        </Hide>
+        <Hide>
+          <Social>
+            <Circle variants={iconAnim}>
+              <Icon src={twitterLogo} alt="twitter-logo" />
+            </Circle>
+            <Link
+              variants={titleAnim}
+              href="https://twitter.com"
+              target="_blank"
+            >
+              Twitter
+            </Link>
+          </Social>
+        </Hide>
+      </div>
+    </StyledContact>
   );
 }
 
+const StyledContact = styled(motion.div)`
+  padding: 5rem 10rem;
+  color: white;
+  height: 90vh;
+  background: url(${photographer}) no-repeat;
+  background-size: cover;
+`;
+
+const Title = styled.div`
+  margin-bottom: 4rem;
+  color: white;
+`;
+
+const Hide = styled.div`
+  overflow: hidden;
+  margin-bottom: 2rem;
+`;
+
+const Circle = styled(motion.div)`
+  border-radius: 50%;
+  width: 6rem;
+  height: 6rem;
+  margin-right: 1rem;
+`;
+
+const Social = styled(motion.div)`
+  display: flex;
+  align-items: center;
+
+  h2 {
+    margin: 2rem;
+  }
+`;
+
+const Icon = styled.img`
+  height: 7rem;
+  width: 7rem;
+  margin: auto;
+`;
+
+const Line = styled(motion.div)`
+  height: 0.5rem;
+  background-color: #23d997;
+  margin-bottom: 3rem;
+  max-width: 50%;
+`;
+
+const Link = styled(motion.a)`
+  text-decoration: none;
+  font-size: 4rem;
+  color: white;
+`;
 export default ContactUs;
