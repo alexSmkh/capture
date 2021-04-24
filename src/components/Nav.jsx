7f3cd777678daac6db7df937c2e38c16426589a9
alexSmkh <alexshikari9333@gmail.com>
2021-04-24 13:02:25 +0300

    Made the navigation block sticky

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -32,6 +32,9 @@ const StyledNav = styled.nav`
   margin: auto;
   padding: 1rem 10rem;
   background-color: #282828;
+  position: sticky;
+  top: 0;
+  z-index: 10;
 
   a {
     text-decoration: none;
